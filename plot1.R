d9611e8f6197e25eb9d640b9f1c5622b030c376d
thomas.lapiana <tal103020@icloud.com>
2016-04-29 11:39:30 -0500

    Added notes

--- plot1.R
+++ plot1.R
@@ -1,2 +1,9 @@
+## Set the working directory and load the required packages
 setwd("/home/thomas/JHU DataScience Courses/R Directory/ExploratoryDataAnalysisFinal")
+library(ggplot2)
+library(data.table)
+library(dplyr)
 
+## Read in the Data 
+NEI <- data.table(readRDS("summarySCC_PM25.rds"))
+SCC <- data.table(readRDS("Source_Classification_Code.rds"))
